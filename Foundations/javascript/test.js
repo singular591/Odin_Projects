c3fa332eea086eb069c0f97a3b48a5c0b5b4b3e2
singular591 <177606829+singular591@users.noreply.github.com>
2024-08-14 15:16:33 +0200

    added some java exercises

---
+++ Foundations/javascript/test.js
@@ -0,0 +1,14 @@
+// let a = Number(prompt("First number?", 1));
+// let b = Number(prompt("Second number?", 2));
+
+
+
+// alert(a + b); // 12
+
+
+let a = Number(prompt("First number?", 1));
+let b = Number(prompt("Second number?", 2));
+
+
+
+alert(a + b); // 12
